2cd8faa8da66feba82235048bfe3f1d989f65e17
saiarepuri2000 <59418637+saiarepuri2000@users.noreply.github.com>
2021-05-23 16:24:17 +0530

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,5 +2,33 @@ import React from 'react';
 import '../App.css';
 
 export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  return (
+     <div className="row">
+          <Experience
+            src = 'images/thailand.jpg'
+            title = "My Experience"
+            content = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled "
+          />
+          <Experience
+            src = 'images/island.jpg'
+            title = "My Experience"
+            content = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled "
+          />
+          <Experience
+            src = 'images/himalayas.jpg'
+            title = "My Experience"
+            content = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled "
+          />
+          <Experience
+            src = 'images/island.jpg'
+            title = "My Experience"
+            content = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled "
+          />
+          <Experience
+            src = 'images/thailand.jpg'
+            title = "My Experience"
+            content = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled"
+          />
+    </div>
+  );
 }
